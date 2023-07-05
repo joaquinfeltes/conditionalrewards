31d7592b6dcdc1c366ae84a6d2792314edd5fb7f
Joaquin Feltes <joacofeltes@gmail.com>
2023-07-05 18:11:44 -0300

    adding more tests

--- test_tad.py
+++ test_tad.py
@@ -11,7 +11,29 @@ from tad import (
     StochasticGame,
     )
 
-
+from tests.node_fixtures import (
+    node_0,
+    node_1_final,
+    node_2_final,
+    node_2_final_eq_rew,
+    node_2,
+    node_s0,
+    node_s1,
+    node_s2,
+    node_s3,
+    node_s4,
+    node_s5_bad,
+    node_s6_good,
+    node_s7_bad,
+    player_node_0,
+    state_list_simple,
+    state_list_two_final_states,
+    state_list_w_player_at_start,
+    state_list_two_final_states_eq_rew,
+    state_list_graph_5_5,
+    transition_matrix_graph_5_5,
+    final_states_graph_5_5,
+    )
 #----------------------------------------ProbabilisticNode----------------------------------------#
    
 def test_probabilistic_node_is_loop_node_false():
@@ -54,57 +76,6 @@ def test_probabilistic_node_is_loop_node_true():
     assert probabilistic_node_loop.is_loop_node == True
 
 
-@pytest.fixture
-def node_0():
-    return ProbabilisticNode(
-        player=PROBABILISTIC,
-        idx=0,
-        reward=1,
-        next_states=[
-            (0.5, 1),
-            (0.5, 2),
-        ],
-        is_final_node=False,
-    )
-
-
-@pytest.fixture
-def node_1_final():
-    return ProbabilisticNode(
-        player=PROBABILISTIC,
-        idx=1,
-        reward=2,
-        next_states=[
-            (1, 1),
-        ],
-        is_final_node=True,
-    )
-
-
-@pytest.fixture
-def node_2():
-    return ProbabilisticNode(
-        player=PROBABILISTIC,
-        idx=2,
-        reward=3,
-        next_states=[
-            (1, 2),
-        ],
-        is_final_node=False,
-    )
-
-
-@pytest.fixture
-def state_list_simple(node_0, node_1_final, node_2):
-    # init the reach_probabily field of the node 1, as it is a final node
-    state_list = [node_0, node_1_final, node_2]
-    for node in state_list:
-        if node.is_final_node:
-            node.reach_probability = 1
-    return state_list
-
-
-
 def test_value_iteration_reach(node_0, state_list_simple):
     assert node_0.value_iteration_reach(state_list_simple) == 0.5
 
@@ -121,52 +92,6 @@ def test_value_iteration_rewards(node_0, state_list_simple):
 
 
 #----------------------------------------PlayerNode----------------------------------------#
-@pytest.fixture
-def player_node_0():
-    player_node_0 = PlayerNode(
-        player=PLAYER_1,
-        idx=0,
-        reward=1,
-        next_states=[
-            ("a", 1),
-            ("b", 2),
-        ],
-        is_final_node=False,
-    )
-    return player_node_0
-
-
-@pytest.fixture
-def state_list_w_player_at_start(player_node_0, node_1_final, node_2):
-    # init the reach_probabily field of the node 1, as it is a final node
-    state_list = [player_node_0, node_1_final, node_2]
-    for node in state_list:
-        if node.is_final_node:
-            node.reach_probability = 1
-    return state_list
-
-
-@pytest.fixture
-def node_2_final():
-    return ProbabilisticNode(
-        player=PROBABILISTIC,
-        idx=2,
-        reward=3,
-        next_states=[
-            (1, 2),
-        ],
-        is_final_node=True,
-    )
-
-
-@pytest.fixture
-def state_list_two_final_states(player_node_0, node_1_final, node_2_final):
-    state_list = [player_node_0, node_1_final, node_2_final]
-    for node in state_list:
-        if node.is_final_node:
-            node.reach_probability = 1
-    return state_list
-
 
 def test_player_node_get_next_state(player_node_0):
     expected_next_state_a = 1
@@ -195,6 +120,9 @@ def test_player_node_get_best_strategies_total_rewards_b_biggest_reward_and_fina
     assert player_node_0.get_best_strategies_total_rewards(state_list_two_final_states) == expected_best_strategies
 
 
+def test_player_node_get_best_strategies_total_rewards_eq_rewards(player_node_0, state_list_two_final_states_eq_rew):
+    expected_best_strategies = ["a","b"]
+    assert player_node_0.get_best_strategies_total_rewards(state_list_two_final_states_eq_rew) == expected_best_strategies
 
 
 def test_player_node_prune_state(player_node_0):
@@ -205,31 +133,113 @@ def test_player_node_prune_state(player_node_0):
 
 
 def test_player_node_value_iteration_reach_max(player_node_0, state_list_w_player_at_start):
-    pass
+    reachability = player_node_0.value_iteration_reach_max(state_list_w_player_at_start)
+    expected_reachability = 1
+    assert reachability == expected_reachability
 
 
+# puedo hacer este test porque la clase player es la misma para player 1 o 2,
+# pero en realidad esta funcion no se usa en el caso de player 1
 def test_player_node_value_iteration_reach_min(player_node_0, state_list_w_player_at_start):
-    pass
+    reachability = player_node_0.value_iteration_reach_min(state_list_w_player_at_start)
+    expected_reachability = 0
+    assert reachability == expected_reachability
+
+
+# in one iteration the reachability is 0, but it will increase in the next iterations
+def test_player_node_value_iteration_reach_full(node_s0, node_s3, node_s4, state_list_graph_5_5):
+    reachability_s0 = node_s0.value_iteration_reach_max(state_list_graph_5_5)
+    reachability_s3 = node_s3.value_iteration_reach(state_list_graph_5_5)
+    reachability_s4 = node_s4.value_iteration_reach(state_list_graph_5_5)
+    expected_reachability_s0 = 0
+    expected_reachability_s3 = 1/2
+    expected_reachability_s4 = 1/2
+    assert reachability_s0 == expected_reachability_s0
+    assert reachability_s3 == expected_reachability_s3
+    assert reachability_s4 == expected_reachability_s4
 
 
 def test_player_node_value_iteration_rewards_max(player_node_0, state_list_w_player_at_start):
-    pass
+    total_rewards = player_node_0.value_iteration_rewards_max(state_list_w_player_at_start)
+    expected_total_rewards = 4
+    assert total_rewards == expected_total_rewards
 
 
 def test_player_node_value_iteration_rewards_min(player_node_0, state_list_w_player_at_start):
-    pass
+    total_rewards = player_node_0.value_iteration_rewards_min(state_list_w_player_at_start)
+    expected_total_rewards = 3
+    assert total_rewards == expected_total_rewards
 
 
+# This are the results after 1 iteration, but the values will be the same in the next iterations
+def test_player_node_value_iteration_rewards_full(
+        node_s0, node_s1, node_s2, node_s3, node_s4,
+        node_s5_bad, node_s6_good, node_s7_bad, 
+        state_list_graph_5_5):
+    rewards_s0 = node_s0.value_iteration_rewards_max(state_list_graph_5_5)
+    rewards_s1 = node_s1.value_iteration_rewards_min(state_list_graph_5_5)
+    rewards_s2 = node_s2.value_iteration_rewards_min(state_list_graph_5_5)
+    rewards_s3 = node_s3.value_iteration_rewards(state_list_graph_5_5)
+    rewards_s4 = node_s4.value_iteration_rewards(state_list_graph_5_5)
+    rewards_s5 = node_s5_bad.value_iteration_rewards(state_list_graph_5_5)
+    rewards_s6 = node_s6_good.value_iteration_rewards(state_list_graph_5_5)
+    rewards_s7 = node_s7_bad.value_iteration_rewards(state_list_graph_5_5)
+    expected_rewards_s0 = 2
+    expected_rewards_s1 = 2
+    expected_rewards_s2 = 5/3
+    expected_rewards_s3 = 0
+    expected_rewards_s4 = 0
+    expected_rewards_s5 = 0
+    expected_rewards_s6 = 0
+    expected_rewards_s7 = 0
+    assert rewards_s0 == expected_rewards_s0
+    assert rewards_s1 == expected_rewards_s1
+    assert rewards_s2 == expected_rewards_s2
+    assert rewards_s3 == expected_rewards_s3
+    assert rewards_s4 == expected_rewards_s4
+    assert rewards_s5 == expected_rewards_s5
+    assert rewards_s6 == expected_rewards_s6
+    assert rewards_s7 == expected_rewards_s7
 
 
 #----------------------------------------Solver----------------------------------------#
 
 
-def test_solver():
+def test_get_reachability_strategies():
     pass
+
+
+def test_solve_reachability_graph_5_5(state_list_graph_5_5, transition_matrix_graph_5_5, final_states_graph_5_5):
+    reachability_strategies = Solver().solve_reachability(
+            state_list=state_list_graph_5_5,
+            transition_matrix=transition_matrix_graph_5_5, 
+            final_states=final_states_graph_5_5)
+    expected_reachability_strategies = [["alfa", "beta"], None, None, None, None, None, None, None]
+    assert reachability_strategies == expected_reachability_strategies
+
+
+def test_get_total_rewards_strategies():
+    pass
+
+
+def test_solve_total_rewards():
+    pass
+
 
 #----------------------------------------StochasticGame----------------------------------------#
 
+#TODO add a test to check that the idx is the same as the index in the list
+#TODO add a test to check that the transition matrix is correct with the function from node_fixtures.py
 
-def test_stochastic_game():
+def test_sg_init_states():
+    pass
+
+
+def test_sg_solve_reachability():
+    pass
+
+def test_sg_solve_total_rewards():
+    pass
+
+def test_solve_sg():
     pass
