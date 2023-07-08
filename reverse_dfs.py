8dd3fa68991a9bcb352bc668699df3092cde3450
Joaquin Feltes <joacofeltes@gmail.com>
2023-07-08 09:44:59 -0300

    fix reverse dfs for new transition structure

--- reverse_dfs.py
+++ reverse_dfs.py
@@ -1,6 +1,18 @@
 # This file contains the functions needed to perform a reverse dfs
 #  on a transition matrix
 
+
+def reverse_dfs(transition_list: list, final_states: list) -> list:
+    transitions = reverse_transition_list(transition_list)
+    reachable_states = []
+    for final_state in final_states:
+        reachable_states = reverse_dfs_recursive(final_state, transitions, reachable_states)
+
+    # remove final_states from reachable_states
+    reachable_states = [state for state in reachable_states if state not in final_states]
+    return reachable_states
+
+
 def reverse_dfs_recursive(state: int, transitions: dict, reachable_states: list) -> list:
     rec_reachable_states = reachable_states.copy()
     rec_reachable_states.append(state)
@@ -11,18 +23,7 @@ def reverse_dfs_recursive(state: int, transitions: dict, reachable_states: list)
     return rec_reachable_states
 
 
-def reverse_dfs(transition_matrix: list, final_states: list) -> list:
-    transitions = reverse_transition_matrix(transition_matrix)
-    reachable_states = []
-    for final_state in final_states:
-        reachable_states = reverse_dfs_recursive(final_state, transitions, reachable_states)
-
-    # remove final_states from reachable_states
-    reachable_states = [state for state in reachable_states if state not in final_states]
-    return reachable_states
-
-
-def reverse_transition_matrix(transition_matrix: list) -> dict:
+def reverse_transition_list(transition_list: list) -> dict:
     """
         The transition matrix has 3 lists, one for each player
         then for each player, if the state is not None in the list the state belongs to that player
@@ -34,22 +35,19 @@ def reverse_transition_matrix(transition_matrix: list) -> dict:
         The reversed transitions will be a dict with the key
         being the state and the value being a list of the states that can be reached
     """
-    reversed_transition_list = reverse_transition_matrix_core(transition_matrix)
+    reversed_transition_list = reverse_transition_list_core(transition_list)
     reversed_transition_dict = list_of_tuples_to_dict_of_lists(reversed_transition_list)
     # The dict might miss some states, add them with empty lists
     reversed_transition_dict = add_missing_states(
-        reversed_transition_dict, len(transition_matrix[0]))
+        reversed_transition_dict, len(transition_list))
     return reversed_transition_dict
 
 
-def reverse_transition_matrix_core(transition_matrix: list) -> list:
+def reverse_transition_list_core(transition_list: list) -> list:
     reversed_transition_list = []
-
-    for player_transitions in transition_matrix:
-        for state_n, state_transitions in enumerate(player_transitions):
-            if state_transitions is not None:
-                for next_state in state_transitions:
-                    reversed_transition_list.append((next_state[1], state_n))
+    for current_state, state_transitions in enumerate(transition_list):
+        for _, next_state in state_transitions:
+            reversed_transition_list.append((next_state, current_state))
     return reversed_transition_list
 
 
--- tests/test_reverse_dfs.py
+++ tests/test_reverse_dfs.py
@@ -1,8 +1,8 @@
 import pytest
 from reverse_dfs import (
     list_of_tuples_to_dict_of_lists,
-    reverse_transition_matrix_core,
-    reverse_transition_matrix,
+    reverse_transition_list_core,
+    reverse_transition_list,
     reverse_dfs,
     )
 # from typing import List, Tuple, Dict
@@ -21,44 +21,42 @@ def test_list_of_tuples_to_dict_of_lists(list_of_tuples):
     assert type(dict_of_lists) == dict
 
 
-# ------------------  test for reverse_transition_matrix_core ----------------------
+# ------------------  test for reverse_transition_list_core ----------------------
 @pytest.fixture
-def transition_matrix():
+def transition_list():
     # from figure 5.4
     return [
-        [[("beta", 1), ("alfa", 2)],  None,  None,  None,  None,  None,  None],
-        [None, None, None, [("delta", 4), ("gamma", 5)], None, None, None],
-        [None, [(3/4, 3), (1/4, 4)], [(1/2, 5), (1/2, 6)], None, [(1, 4)],
-         [(1, 5)], [(1, 6)]],
+        [("beta", 1), ("alfa", 2)],  [(3/4, 3), (1/4, 4)], [(1/2, 5), (1/2, 6)],
+        [("delta", 4), ("gamma", 5)], [(1, 4)], [(1, 5)], [(1, 6)]
     ]
 
 
-def test_reverse_transition_matrix_core(transition_matrix):
-    reverse_transition_list = reverse_transition_matrix_core(transition_matrix)
+def test_reverse_transition_list_core(transition_list):
+    reverse_transition_list = reverse_transition_list_core(transition_list)
     expected_reverse_transition_list = [
-        (1, 0), (2, 0), (4, 3), (5, 3), (3, 1), (4, 1),
-        (5, 2), (6, 2), (4, 4), (5, 5), (6, 6)]
+        (1, 0), (2, 0), (3, 1), (4, 1), (5, 2), (6, 2), (4, 3), (5, 3), (4, 4), (5, 5), (6, 6)
+        ]
     assert reverse_transition_list == expected_reverse_transition_list
     assert type(reverse_transition_list) == list
 
-# ------------------  test for reverse_transition_matrix ----------------------
+# ------------------  test for reverse_transition_list ----------------------
 
 
-def test_reverse_transition_matrix(transition_matrix):
-    reversed_transitions = reverse_transition_matrix(transition_matrix)
+def test_reverse_transition_list(transition_list):
+    reversed_transitions = reverse_transition_list(transition_list)
     expected_reversed_transitions = {
-        1: [0], 2: [0], 4: [3, 1, 4], 5: [3, 2, 5], 3: [1], 6: [2, 6], 0: []}
+        1: [0], 2: [0], 3: [1], 4: [1, 3, 4], 5: [2, 3, 5], 6: [2, 6], 0: []
+        }
     assert reversed_transitions == expected_reversed_transitions
     assert type(reversed_transitions) == dict
 
 
 # ------------------  test for reverse_dfs ------------------------------------
 @pytest.fixture
-def transition_matrix_state_two_can_not_reach_state_five():
+def transition_list_state_two_can_not_reach_state_five():
     return [
-        [[("beta", 1), ("alfa", 2)],  None,  None,  None,  None,  None,  None],
-        [None, None, None, [("delta", 4), ("gamma", 5)], None, None, None],
-        [None, [(3/4, 3), (1/4, 4)], [(1, 6)], None, [(1, 4)], [(1, 5)], [(1, 6)]],
+        [("beta", 1), ("alfa", 2)],  [(3/4, 3), (1/4, 4)], [(1, 6)],
+        [("delta", 4), ("gamma", 5)], [(1, 4)], [(1, 5)], [(1, 6)]
     ]
 
 
@@ -67,16 +65,16 @@ def final_states():
     return [5]
 
 
-def test_reverse_dfs(transition_matrix, final_states):
-    reachable_states = reverse_dfs(transition_matrix, final_states)
+def test_reverse_dfs(transition_list, final_states):
+    reachable_states = reverse_dfs(transition_list, final_states)
     expected_reachable_states = [0, 1, 2, 3]
     assert sorted(reachable_states) == expected_reachable_states
     assert type(reachable_states) == list
 
 
 def test_reverse_dfs_state_two_can_not_reach(
-        transition_matrix_state_two_can_not_reach_state_five, final_states):
+        transition_list_state_two_can_not_reach_state_five, final_states):
     reachable_states = reverse_dfs(
-        transition_matrix_state_two_can_not_reach_state_five, final_states)
+        transition_list_state_two_can_not_reach_state_five, final_states)
     expected_reachable_states = [0, 1, 3]
     assert sorted(reachable_states) == expected_reachable_states
