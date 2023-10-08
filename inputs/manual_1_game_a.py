1a7aa0d42397c626debdfcc261d03bd3bc967842
Joaquín Feltes <joacofeltes@gmail.com>
2023-10-08 13:30:44 -0300

    new roberta runs

---
+++ inputs/manual_1_game_a.py
@@ -0,0 +1,85 @@
+# Board:
+#
+#   [0|<-( )] [5|->( )] [5|<-( )] [3|<-(X)]
+#   [1|<>( )] [0|<-( )] [0|<-(X)] [4|<>( )]
+#   [5|->( )] [2|->( )] [0|<-( )] [0|<>( )]
+#   [2|<>( )] [2|<>( )] [0|->(X)] [0|->( )]
+
+#LOS UNICOS ESTADOS CON REACHABILITY SON LOS ULTIMOS QUE TIENEN LA OPCION DE IR ABAJO
+# 28, 29, 30, 31.
+# Y LOS QUE VAN A ESTOS SON LOS "GREEN" DEL JUGADOR 2 DE ESA ULTIMA FILA.
+# LO QUE PASA ES QUE AL MINIMIZAR LA ALCANZABILIDAD, 
+# NUNCA ELIGE GREEN (EN NUESTRO ALGORITMO)
+
+{
+ 'game_a': {'rewards': [0, 5, 5, 3, 1, 0, 0, 4, 5, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'players': ['Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 2', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Player 1', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic', 'Probabilistic'],
+            'transition_list': [
+                [('Green', 16), ('Yellow', 32)],
+                [('Green', 17), ('Yellow', 33)],
+                [('Green', 18), ('Yellow', 34)],
+                [('Green', 19), ('Yellow', 35)],
+                [('Green', 20), ('Yellow', 36)],
+                [('Green', 21), ('Yellow', 37)],
+                [('Green', 22), ('Yellow', 38)],
+                [('Green', 23), ('Yellow', 39)],
+                [('Green', 24), ('Yellow', 40)],
+                [('Green', 25), ('Yellow', 41)],
+                [('Green', 26), ('Yellow', 42)],
+                [('Green', 27), ('Yellow', 43)],
+                [('Green', 28), ('Yellow', 44)],
+                [('Green', 29), ('Yellow', 45)],
+                [('Green', 30), ('Yellow', 46)],
+                [('Green', 31), ('Yellow', 47)],
+                [('Down', 52)],
+                [('Down', 53)],
+                [('Down', 54)],
+                [('Down', 55)],
+                [('Down', 56)],
+                [('Down', 57)],
+                [('Down', 58)],
+                [('Down', 59)],
+                [('Down', 60)],
+                [('Down', 61)],
+                [('Down', 62)],
+                [('Down', 63)],
+                [('Down', 65)],
+                [('Down', 65)],
+                [('Down', 65)],
+                [('Down', 65)],
+                [('Left', 51)],
+                [('Right', 50)],
+                [('Left', 49)],
+                [('Left', 50)],
+                [('Left', 55), ('Right', 53)],
+                [('Left', 52)],
+                [('Left', 53)],
+                [('Left', 54), ('Right', 52)],
+                [('Right', 57)],
+                [('Right', 58)],
+                [('Left', 57)],
+                [('Left', 58), ('Right', 56)],
+                [('Left', 63), ('Right', 61)],
+                [('Left', 60), ('Right', 62)],
+                [('Right', 63)],
+                [('Right', 60)],
+                [(1, 0)],
+                [(1, 1)],
+                [(1, 2)],
+                [(0.1, 64), (0.9, 3)],
+                [(1, 4)],
+                [(1, 5)],
+                [(0.1, 64), (0.9, 6)],
+                [(1, 7)],
+                [(1, 8)],
+                [(1, 9)],
+                [(1, 10)],
+                [(1, 11)],
+                [(1, 12)],
+                [(1, 13)],
+                [(0.1, 64), (0.9, 14)],
+                [(1, 15)],
+                [(1, 64)],
+                [(1, 65)]],
+            'final_states': [65]}
+}
